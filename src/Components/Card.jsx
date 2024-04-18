44ea137cd398fe224c6b86b448af847f957ba902
Yashgupta9330 <yaahg342@gmail.com>
2024-04-18 23:18:44 +0530

    added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -11,7 +11,7 @@ export default function Card(){
         </div>
          <div className="flex flex-col w-[544px] h-full text-[#20294C]">
             <span className="font-semibold text-[60px] py-[20px] border leading-60 tracking-[-1.92px]" style={{fontFamily: 'Source Code Pro, monospace' }}>Stop worrying about exams</span>
-            <p className="py-[20px] font-regular text-[24px] leading-9 text-left">
+            <p className="py-[20px] font-regular text-[24px] leading-9 text-left font-inter">
                 ZuAI app is your new study buddy that hels you <span className="font-bold">prepare,practise,revise and learn smartly </span>Now you can
                 mastering your subjects rather than judging study tasks
             </p>
@@ -23,9 +23,9 @@ export default function Card(){
         </div>
          <div className="flex flex-col w-[544px] h-full text-[#20294C]">
             <span className="font-semibold text-[60px] py-[20px] border leading-60 tracking-[-1.92px]" style={{fontFamily: 'Source Code Pro, monospace' }}>Stop bored- Studying</span>
-            <p className="py-[20px] font-regular text-[24px] leading-9 text-left">
-                ZuAI app is your new study buddy that hels you <span className="font-bold">prepare,practise,revise and learn smartly </span>Now you can
-                mastering your subjects rather than judging study tasks
+            <p className="py-[20px] font-regular text-[24px] leading-9 text-left font-inter">
+            Textbooks are boring, you will sleep faster than learning concepts and clear doubts. With ZuAI, say hello to  <span className="font-bold border bg-[#20294C]">interactive learning experiences </span>
+             that make you forget you're even studying. Who knew learning could be this fun (or that textbooks could be this jealous)
             </p>
         </div>
         </div>
@@ -35,9 +35,9 @@ export default function Card(){
         </div>
          <div className="flex flex-col w-[544px] h-full text-[#20294C]">
             <span className="font-semibold text-[60px] py-[20px] border leading-60 tracking-[-1.92px]" style={{fontFamily: 'Source Code Pro, monospace' }}>Stop Feeling Lonely</span>
-            <p className="py-[20px] font-regular text-[24px] leading-9 text-left">
-                ZuAI app is your new study buddy that hels you <span className="font-bold">prepare,practise,revise and learn smartly </span>Now you can
-                mastering your subjects rather than judging study tasks
+            <p className="py-[20px] font-regular text-[24px] leading-9 text-left font-inter">
+            Remember that angry time, when no one was Remember that angry time, when no one was parent, and no friends? ZuAI is 24X7 there for you, 
+            it's a teacher that you always wanted and <span className='text-xs'> friend you never had.</span>
             </p>
         </div>
         </div>
@@ -47,9 +47,9 @@ export default function Card(){
         </div>
          <div className="flex flex-col w-[544px] h-full text-[#20294C]">
             <span className="font-semibold text-[60px] py-[20px] border leading-60 tracking-[-1.92px]" style={{fontFamily: 'Source Code Pro, monospace' }}>Stop Wasting your Time</span>
-            <p className="py-[20px] font-regular text-[24px] leading-9 text-left">
-                ZuAI app is your new study buddy that hels you <span className="font-bold">prepare,practise,revise and learn smartly </span>Now you can
-                mastering your subjects rather than judging study tasks
+            <p className="font-inter py-[20px] font-regular text-[24px] leading-9 text-left">
+             Don't wait for someone to solve your doubts or go through the same things again and again to <div className='border bg-[#20294C]'><span className="font-bold">get a follow-up doubt cleared. ZuAI solves any doubt for every difficulty you face on the spot </span></div>
+             so that you don't waste time and keep going effectively.
             </p>
         </div>
         </div>
