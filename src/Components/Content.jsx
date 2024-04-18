32e844c6466826bc69e63ff52e6d9c072f914f33
Yashgupta9330 <yaahg342@gmail.com>
2024-04-19 02:44:10 +0530

    added

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -4,8 +4,8 @@ import center from "../assets/center.jpg"
 import { FaArrowRight } from "react-icons/fa6";
 export default function Content(){
     return(
-        <div className="w-full px-[80px] flex flex-col items-center gap-[32px] h-[1289px] relative">
-         <h1 className="text-6xl xl:text-[72px] font-extrabold" style={{fontFamily: 'Source Code Pro, monospace' }}>Introducing Nikaru Widgets!</h1>
+        <div className="w-full px-20 flex flex-col items-center gap-[32px] relative mb-32">
+         <h1 className="text-5xl xl:text-[72px] font-extrabold" style={{fontFamily: 'Source Code Pro, monospace' }}>Introducing Nikaru Widgets!</h1>
          <div className="font-regular text-[18px] flex flex-col items-center justify-center">
             <p>Sit elit feugiat turpis sed integer integer accumsan turpis. Sed suspendisse nec lorem mauris.</p>
             <p>Pharetra, eu imperdiet ipsum ultrices amet, dui sit suspendisse.</p>
@@ -14,9 +14,9 @@ export default function Content(){
         <span className="font-bold">Start for Free</span>
         <FaArrowRight  className="font-bold text-[20px] leading-[100%]"/>
         </button>
-        <img src={center} alt="center" className="w-[900px] z-[1]"/>
-        <img src={left} alt="left"  className="absolute left-0 top-[160px] w-[300px]" />
-        <img src={right1} alt="right"  className="absolute right-0 bottom-[400px] w-[600px]" />
+        <img src={center} alt="center" className="w-[70%] z-[1]"/>
+        <img src={left} alt="left"  className="absolute left-0 top-[160px] w-[25%] xl:w-[300px]" />
+        <img src={right1} alt="right"  className="absolute right-0 bottom-0 w-[50%] xl:w-[600px]" />
         </div>
     );
 }
--- src/Components/Download.jsx
+++ src/Components/Download.jsx
@@ -4,13 +4,13 @@ import Plays from "./Play"
 
 export default function Download(){
     return(
-        <div className="w-[90%] px-[80px] py-[80px]  h-[400px] bg-[#7C3AED] rounded-lg flex items-center justify-center relative">
-        <img src={left3} alt="left3" className="w-[500px] left-[-60px] bottom-0 absolute "/> 
+        <div className="w-[95%] px-20  py-12 xl:py-20 bg-[#7C3AED] rounded-lg flex items-center justify-center relative overflow-hidden">
+        <img src={left3} alt="left3" className="w-2/5 left-[-60px] bottom-0 absolute"/> 
         <div className="flex flex-col items-center justify-center z-[1] gap-20">
             <h1 className="font-extrabold text-[56px] text-[#FFFFFF]">Download Now!</h1>
             <Plays/>
         </div>
-        <img src={left4} alt="left3" className="absolute right-[-36px] bottom-0"/> 
+        <img src={left4} alt="left3" className="absolute w-1/2 right-[-36px] bottom-0"/> 
         </div>
     )
 }
