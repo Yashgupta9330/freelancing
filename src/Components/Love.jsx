e529cee3e85c2cbf14558037677c30c8b7b41e6c
Yashgupta9330 <yaahg342@gmail.com>
2024-04-21 04:28:56 +0530

    added

--- src/Components/Love.jsx
+++ src/Components/Love.jsx
@@ -1,11 +1,12 @@
-import { FaHeart } from "react-icons/fa6";;
+import { FaHeart } from "react-icons/fa6";import { Link } from "react-router-dom";
+;
 
 export default function Love(){
     return(
         <div className="flex border rounded-lg px-4 py-2 bg-purple-600">
         <button className="flex items-center justify-center gap-6 text-[#FFFFFF] leading-6 tracking-tighter">
         <FaHeart />
-         <a href="/board"><span>Nikaru Love Board</span></a>
+         <Link to="/board"><span>Nikaru Love Board</span></Link>
         </button>
        </div>
     )
