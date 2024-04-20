b89ff81fe7a7004e18a1e8f2cca89619cc3e4ee4
Yashgupta9330 <yaahg342@gmail.com>
2024-04-21 04:16:09 +0530

    changed

--- src/Components/Love.jsx
+++ src/Components/Love.jsx
@@ -5,7 +5,7 @@ export default function Love(){
         <div className="flex border rounded-lg px-4 py-2 bg-purple-600">
         <button className="flex items-center justify-center gap-6 text-[#FFFFFF] leading-6 tracking-tighter">
         <FaHeart />
-         <a href="/love"><span>Nikaru Love Board</span></a>
+         <a href="/board"><span>Nikaru Love Board</span></a>
         </button>
        </div>
     )
