fe7e7a885ca07cc793c57913b86577dec68e9384
Yashgupta9330 <yaahg342@gmail.com>
2024-04-26 17:08:55 +0530

    added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,46 +12,43 @@ import Youtube from './Youtube';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Carausel from './Caraursel';
-import bgChat from "../assets/bgChat.png";
+import bgChat2 from "../assets/bgChat2.png";
 
 export default function Home() {
-    useEffect(() => {
-        AOS.init();
-        AOS.refresh();
-    }, []);
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
 
-    // State to hold the height dynamically
-    const [contentHeight, setContentHeight] = useState(720); // Default height, you can change as needed
-
-    return (
-        <>
-            <div className='w-full min-h-screen overflow-x-hidden'>
-                <div className=''>
-                    {/* Dynamically set height and background image */}
-                    <div className='bg-[url(bgChat)]  bg-cover bg-center z-[100] w-full h-[750px] sm:h-[720px]' style={{ backgroundImage: `url(${bgChat})` }}>
-                        <div className='w-full flex items-center justify-center'>
-                            <Navbar />
-                        </div>
-                        <div className="w-full flex flex-col items-center justify-center rounded-lg relative mt-32">
-                            <Logo classname='h-[253px] w-[253px] ' />
-                            <Plays />
-                            <AI />
-                        </div>
-                    </div>
-                </div>
-                <Carausel />
-                <div className="w-full flex flex-col items-center justify-center mb-12 mt-12">
-                    <Card />
-                </div>
-                <Content />
-                <Youtube />
-                <div className="w-full flex flex-col items-center justify-center mb-12 mt-12">
-                    <Download />
-                </div>
-                <div className="w-full flex flex-col items-center justify-center mb-4 mt-4">
-                    <Footer />
-                </div>
+  
+  return (
+    <>
+      <div className='w-full min-h-screen overflow-x-hidden'>
+        <div className='bg-[#F0F1F5]'>
+          <div className='flex flex-col gap-32 z-[100] bg-repeat bg-contain' style={{ backgroundImage: `url(${bgChat2})`}}>
+            <Navbar />
+            <div className='mt-32'>
+            <div className="w-full flex flex-col items-center justify-center rounded-lg ">
+              <Logo className='h-[253px] w-[253px]' />
+              <Plays />
+              <AI />
             </div>
-        </>
-    )
+          </div>
+        </div>
+        </div>
+        <Carausel />
+        <div className="w-full flex flex-col items-center justify-center mb-12 mt-12">
+          <Card />
+        </div>
+        <Content />
+        <Youtube />
+        <div className="w-full flex flex-col items-center justify-center mb-12 mt-12">
+          <Download />
+        </div>
+        <div className="w-full flex flex-col items-center justify-center mb-4 mt-4">
+          <Footer />
+        </div>
+      </div>
+    </>
+  )
 }
