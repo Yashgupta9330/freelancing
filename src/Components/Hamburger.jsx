ea7c469e8e37baa9c19c6b2dbf2250465e92a9c6
Yashgupta9330 <yaahg342@gmail.com>
2024-04-27 01:40:32 +0530

    added

--- src/Components/Hamburger.jsx
+++ src/Components/Hamburger.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import Love from "./Love";
+import Navbar from "./Navbar";
 
 export default function Hamburger() {
   const [nav, setNav] = useState(false);
@@ -9,12 +10,24 @@ export default function Hamburger() {
   return (
     <div className="z-[150] w-[90%] md:w-[95%] min-h-[144px] flex items-center justify-end font-roboto mr-0 md:ml-4 md:ml-0 fixed top-0 left-0">
       <div onClick={() => setNav(!nav)} className="rounded-full bg-black text-[#FFFFFF] flex items-center justify-center px-4 py-4">
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      {!nav ? (
+       <AiOutlineMenu size={20} />
+       ) : (
+     <>
+    <div className="flex flex-col gap-4 hidden md:block">
+    <Navbar />
+    <AiOutlineClose size={20} />
+    </div>
+    <div className="block md:hidden">
+    <AiOutlineClose size={20} />
+    </div>
+  </>
+)}
       </div>
       <ul
         className={
           nav
-            ? "fixed left-0 top-0 w-[60%] md:w-[30%] h-full border-r text-white border-r-gray-900 bg-[#28143a] ease-in-out duration-500 z-50"
+            ? "fixed md:hidden left-0 top-0 w-[60%] md:w-[30%] h-full border-r text-white border-r-gray-900 bg-[#28143a] ease-in-out duration-500 z-50"
             : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
         }
       >
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   const navItems = ["About us", "Board of Love", "Plugins", "Contact Us"];
 
   return (
-    <div className="z-[150] w-full min-h-[144px]  flex items-center justify-center font-roboto mr-0 md:ml-4 md:ml-0 fixed top-0 left-0">
+    <div className="z-[150] w-full min-h-[144px]  flex items-center justify-center font-roboto mr-0 md:ml-0 fixed top-0 left-0">
       <div className=" w-[95%] lg:w-4/5 min-h-[85px] rounded-lg bg-[#8576FF] bg-opacity-100 py-[24px] px-[12px] md:px-[24px] box">
         <div className="flex items-center justify-between">
           <div className="flex gap-8 text-black">
