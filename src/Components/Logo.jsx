450ada122593a5ca537a4c476c60f3578b1262ff
Yashgupta9330 <yaahg342@gmail.com>
2024-05-01 14:59:54 +0530

    added

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,20 +1,11 @@
 import { useEffect, useState } from 'react';
 import output from "../assets/output.gif";
-import logo2 from "../assets/logo2.png";
+import logo2 from "../assets/logo2.svg";
 
 export default function Logo({ classname, foot = '0' }) {
   
     return (
         <>
-         {/*   {foot === '0' ? (
-                <img
-                    src={output}
-                    alt="logo"
-                    className="w-[300px] h-[300px]"
-                />
-            ) : (
-                <img src={logo2} className={classname} />
-            )}  */}
             <img src={logo2} className={classname} />
         </>
     );
