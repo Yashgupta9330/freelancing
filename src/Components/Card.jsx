2a5c6442591072c483f485ea54fe67a21f205039
Yashgupta9330 <yaahg342@gmail.com>
2024-05-01 16:17:14 +0530

    added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -35,131 +35,127 @@ export default function Card() {
   }, [inView1, inView2, inView3, inView4, viewed1, viewed2, viewed3, viewed4]);
 
   return (
-    <div className="md:max-w-[1104px] w-[95%] flex flex-col items-center justify-center gap-32">
-      {/* First Card */}
-      <div className="md:w-full w-[95%] flex flex-col md:flex-row sm:items-center justify-start items-start md:justify-between gap-12 mt-12">
-        <div ref={ref1}>
-          {viewed1 && (
-            <div className="pl-2 md:px-0 flex flex-col items-start justify-start w-[95%] md:w-[544px] text-[#20294C]">
-              <TypeAnimation
-                sequence={["AI-Powered Knowledge Retrieval", 1000]}
-                cursor={true}
-                className="font-semibold text-4xl lg:text-6xl py-2 lg:py-5 leading-30 lg:leading-60 tracking-[-1.92px]"
-                style={{
-                  whiteSpace: "pre-line",
-                  display: "block",
-                  fontFamily: "Source Code Pro, monospace",
-                }}
-                omitDeletionAnimation={true}
-              />
-              <p
-                data-aos="fade-up"
-                data-aos-delay={viewed1 ? 1000 : 0}
-                data-aos-once="true"
-                className="py-2 px-2 lg:py-5 font-regular text-xl lg:text-2xl leading-9 md:text-left font-inter"
-              >
-                Nikaru harnesses the power of AI to decode handwritten notes,
-                decipher complex passages, and provide instant answers.
-              </p>
-            </div>
-          )}
-        </div>
-        {inView1 && (
-          <div className="w-full sm:w-[80%] lg:w-[50%] min-h-[250px] sm:min-h-[400px] lg:min-h-[370px] relative px-2 py-2">
-          <div className="absolute left-0 top-[10%] lg:top-[30%] xl:top-[20%] translate-y-1/2" data-aos="fade-left" data-aos-duration="1000">
-         <img src={Group8301} alt="Group" className="w-full" />
-       </div>
-       <div className="absolute left-1/2 top-[-10%] lg:top-[10%] xl:top-[-10%]" data-aos="fade-up" data-aos-duration="1000">
-         <img src={Iphones} alt="iPhones" className="w-[250px] z-[100]" />
-        </div>
-       </div>
-        )}
-      </div>
-
-      {/* Second Card */}
-      <div className="flex flex-col md:flex-row-reverse sm:items-center justify-center md:justify-between w-[95%] gap-12">
-        <div ref={ref2}>
-          {viewed2 && (
-            <div
-              ref={ref2}
-              className="pl-4 md:px-0 flex flex-col md:w-[544px] text-[#20294C]"
+    <div className="md:max-w-[1104px] w-[95%] flex flex-col items-center justify-center gap-32 md:gap-16 lg:gap-32">
+    {/* First Card */}
+    <div className="md:w-full w-[95%] flex flex-col md:flex-row md:items-center justify-start items-start md:justify-between gap-12 mt-12">
+      <div ref={ref1} className="flex-grow md:w-1/2 pl-4 md:px-0 text-[#20294C]">
+        {viewed1 && (
+            <div>
+            <TypeAnimation
+              sequence={["AI-Powered Knowledge Retrieval", 1000]}
+              cursor={true}
+              className="font-semibold text-4xl lg:text-6xl py-2 lg:py-5 leading-30 lg:leading-60 tracking-[-1.92px]"
+              style={{
+                whiteSpace: "pre-line",
+                display: "block",
+                fontFamily: "Source Code Pro, monospace",
+              }}
+              omitDeletionAnimation={true}
+            />
+            <p
+              data-aos="fade-up"
+              data-aos-delay={viewed1 ? 1000 : 0}
+              data-aos-once="true"
+              className="py-2 px-2 lg:py-5 font-regular text-xl lg:text-2xl leading-9 md:text-left font-inter"
             >
-              <TypeAnimation
-                sequence={["Global Reach,Local Understanding", 1000]}
-                cursor={true}
-                className="font-semibold text-4xl lg:text-6xl py-2 lg:py-5 leading-30 lg:leading-60 tracking-[-1.92px]"
-                style={{
-                  whiteSpace: "pre-line",
-                  display: "block",
-                  fontFamily: "Source Code Pro, monospace",
-                }}
-                omitDeletionAnimation={true}
-              />
-              <p
-                data-aos="fade-up"
-                data-aos-delay={viewed2 ? 1000 : 0}
-                data-aos-once="true"
-                className="py-2 lg:py-5 font-regular text-xl lg:text-2xl leading-9 text-left font-inter"
-              >
-                Nikaru breaks language barriers with its multilingual support,
-                ensuring that users can access answers and insights in their
-                preferred language, regardless of location or linguistic
-                background.
-              </p>
-            </div>
-          )}
-        </div>
-        {inView2 && (
-          <div className="w-full sm:w-[80%] lg:w-[50%] min-h-[250px] sm:min-h-[400px] lg:min-h-[370px] relative px-2 py-2">
-          <div className="absolute left-0 top-[10%] lg:top-[30%] xl:top-[20%] translate-y-1/2" data-aos="fade-right" data-aos-duration="1000">
-         <img src={Group8304} alt="Group" className="w-full" />
-       </div>
-       <div className="absolute left-1/2 top-[-10%] lg:top-[10%] xl:top-[-10%]" data-aos="fade-up" data-aos-duration="1000">
-         <img src={Iphones} alt="iPhones" className="w-[250px] z-[100]" />
-       </div>
-       </div>
+              Nikaru harnesses the power of AI to decode handwritten notes,
+              decipher complex passages, and provide instant answers.
+            </p>
+          </div>
         )}
       </div>
-
-      {/* Third Card */}
-      <div className="flex flex-col md:flex-row sm:items-center justify-center md:justify-between w-[95%] gap-16">
-        <div ref={ref3}>
-          {viewed3 && (
-            <div className="pl-4 md:px-0 flex flex-col md:w-[544px] text-[#20294C]">
-              <TypeAnimation
-                sequence={["Real-Time Insights", 1000]}
-                cursor={true}
-                className="font-semibold text-4xl lg:text-6xl py-2 lg:py-5 leading-30 lg:leading-60 tracking-[-1.92px]"
-                style={{
-                  whiteSpace: "pre-line",
-                  display: "block",
-                  fontFamily: "Source Code Pro, monospace",
-                }}
-                omitDeletionAnimation={true}
-              />
-              <p
-                data-aos="fade-up"
-                data-aos-delay={viewed3 ? 1000 : 0}
-                data-aos-once="true"
-                className="py-2 md:py-5 font-regular text-xl lg:text-2xl leading-9 text-left font-inter"
-              >
-                Stay current with Nikaru, offering instant access to the latest
-                knowledge from across the web, ensuring you're always
-                up-to-date.
-                {/* <span className='text-xs'> friend you never had.</span> */}
-              </p>
-            </div>
-          )}
-        </div>
-        <div
-          data-aos="fade-left"
-          data-aos-duration="1000"
-          className="px-4"
-        >
-          <img src={Group8303} alt="image-3" className="w-[557px]" />
-        </div>
+      {inView1 && (
+        <div className="w-full sm:w-[80%] md:w-[50%] min-h-[250px] sm:min-h-[400px] lg:min-h-[370px] relative px-2 py-2">
+        <div className="absolute left-0 top-[10%] md:top-0 lg:top-[30%] xl:top-[20%] translate-y-1/2" data-aos="fade-left" data-aos-duration="1000">
+       <img src={Group8301} alt="Group" className="w-full" />
+     </div>
+     <div className="absolute left-1/2 top-[-10%] md:top-[20%] lg:top-[10%] xl:top-[-10%]" data-aos="fade-up" data-aos-duration="1000">
+       <img src={Iphones} alt="iPhones" className="w-[250px] z-[100]" />
       </div>
+     </div>
+      )}
+    </div>
 
+    {/* Second Card */}
+    <div className="flex flex-col md:flex-row-reverse items-center justify-center md:justify-between w-[95%] gap-12">
+      <div ref={ref2} className="flex-grow md:w-1/2 pl-4 md:px-0 text-[#20294C]">
+        {viewed2 && (
+           <div>
+            <TypeAnimation
+              sequence={["Global Reach,Local Understanding", 1000]}
+              cursor={true}
+              className="font-semibold text-4xl lg:text-6xl py-2 lg:py-5 leading-30 lg:leading-60 tracking-[-1.92px]"
+              style={{
+                whiteSpace: "pre-line",
+                display: "block",
+                fontFamily: "Source Code Pro, monospace",
+              }}
+              omitDeletionAnimation={true}
+            />
+            <p
+              data-aos="fade-up"
+              data-aos-delay={viewed2 ? 1000 : 0}
+              data-aos-once="true"
+              className="py-2 lg:py-5 font-regular text-xl lg:text-2xl leading-9 text-left font-inter"
+            >
+              Nikaru breaks language barriers with its multilingual support,
+              ensuring that users can access answers and insights in their
+              preferred language, regardless of location or linguistic
+              background.
+            </p>
+            </div>
+        )}
+      </div>
+      {inView2 && (
+        <div className="w-full sm:w-[80%] md:w-[50%] min-h-[250px] sm:min-h-[400px] lg:min-h-[370px] relative px-2 py-2">
+        <div className="absolute left-0 top-[10%] md:top-[40%] lg:top-[30%] xl:top-[20%] translate-y-1/2" data-aos="fade-right" data-aos-duration="1000">
+       <img src={Group8304} alt="Group" className="w-full" />
+     </div>
+     <div className="absolute left-1/2 top-[-10%] md:top-[20%] lg:top-[10%] xl:top-[-10%]" data-aos="fade-up" data-aos-duration="1000">
+       <img src={Iphones} alt="iPhones" className="w-[250px] z-[100]" />
+     </div>
+     </div>
+      )}
+    </div>
+
+    {/* Third Card */}
+    <div className="flex flex-col md:flex-row sm:items-center justify-center md:justify-between w-[95%] gap-16">
+      <div ref={ref3}>
+        {viewed3 && (
+          <div className="pl-4 md:px-0 flex flex-col md:w-[544px] text-[#20294C]">
+            <TypeAnimation
+              sequence={["Real-Time Insights", 1000]}
+              cursor={true}
+              className="font-semibold text-4xl lg:text-6xl py-2 lg:py-5 leading-30 lg:leading-60 tracking-[-1.92px]"
+              style={{
+                whiteSpace: "pre-line",
+                display: "block",
+                fontFamily: "Source Code Pro, monospace",
+              }}
+              omitDeletionAnimation={true}
+            />
+            <p
+              data-aos="fade-up"
+              data-aos-delay={viewed3 ? 1000 : 0}
+              data-aos-once="true"
+              className="py-2 md:py-5 font-regular text-xl lg:text-2xl leading-9 text-left font-inter"
+            >
+              Stay current with Nikaru, offering instant access to the latest
+              knowledge from across the web, ensuring you're always
+              up-to-date.
+              {/* <span className='text-xs'> friend you never had.</span> */}
+            </p>
+          </div>
+        )}
+      </div>
+      <div
+        data-aos="fade-left"
+        data-aos-duration="1000"
+        className="px-4"
+      >
+        <img src={Group8303} alt="image-3" className="w-[557px]" />
+      </div>
+    </div>
       {/* Fourth Card */}
       <div className="flex flex-col md:flex-row-reverse  items-center justify-center md:justify-between w-[95%] gap-12 md:mb-16">
         <div ref={ref4} className="flex-grow md:w-1/2 pl-4 md:px-0 text-[#20294C]">
@@ -190,15 +186,15 @@ export default function Card() {
           )}
         </div>
         {inView4 && (
-  <div className="w-full sm:w-[80%] lg:w-[50%] min-h-[250px] sm:min-h-[400px] lg:min-h-[370px] relative px-2 py-2">
-    <div className="absolute left-0 top-[10%] lg:top-[30%] xl:top-[20%] translate-y-1/2" data-aos="fade-right" data-aos-duration="1000">
-      <img src={Group8302} alt="Group" className="w-full" />
-    </div>
-    <div className="absolute left-1/2 top-[-10%] lg:top-[10%] xl:top-[-10%]" data-aos="fade-up" data-aos-duration="1000">
-      <img src={iPhone} alt="iPhones" className="w-[250px] z-[100]" />
-    </div>
-  </div>
-)}
+        <div className="w-full sm:w-[80%] md:w-[50%] min-h-[300px] sm:min-h-[400px] lg:min-h-[370px] relative px-2 py-2 mb-24">
+        <div className="absolute left-0 top-[10%] md:top-[40%] lg:top-[30%]  xl:top-[20%] translate-y-1/2" data-aos="fade-right" data-aos-duration="1000">
+        <img src={Group8302} alt="Group" className="w-full" />
+        </div>
+        <div className="absolute left-1/2 top-[-10%] md:top-[20%] lg:top-[10%] xl:top-[-10%]" data-aos="fade-up" data-aos-duration="1000">
+         <img src={iPhone} alt="iPhones" className="w-[400px] sm:w-[250px] z-[100]" />
+         </div>
+       </div>
+     )}
 
       </div>
     </div>
